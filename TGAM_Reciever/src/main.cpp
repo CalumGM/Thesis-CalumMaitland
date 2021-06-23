a7b02683637838fb7e583d4c19ee3e3a4be539ca
Calum_Maitland <calum.maitland00@gmail.com>
2021-06-23 16:16:15 +1000

    reading TGAM over Serial

---
+++ TGAM_Reciever/src/main.cpp
@@ -0,0 +1,27 @@
+#include <Arduino.h>
+#include <SoftwareSerial.h>
+
+#define rxPin 13
+#define txPin 12
+SoftwareSerial mySerial(rxPin, txPin); // RX, TX
+char myChar;
+int i = 0;
+void setup()
+{
+  Serial.begin(57600);
+  Serial.println("Beginning");
+  mySerial.begin(57600);
+}
+void loop()
+{
+
+  while (mySerial.available())
+  {
+    
+    myChar = mySerial.read();
+    Serial.print(myChar);
+    if (i == 8){
+      Serial.print("\n");
+    }
+  }
+}
